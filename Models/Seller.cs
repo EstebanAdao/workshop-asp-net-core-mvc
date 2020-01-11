74dcaa74f67ed8c6dd442014b2700a91f88685cb
Esteban Perea <mondragon1669@gmail.com>
2020-01-11 20:01:55 -0300

    MySql and Migration

---
+++ Models/Seller.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMvc.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BrithDate { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+        public ICollection<SalesRecord> SalesRecords { get; set; } = new List<SalesRecord>();
+
+        public Seller()
+        {
+        }
+
+        public Seller(int id, string name, string email, DateTime brithDate, double baseSalary, Department department)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BrithDate = brithDate;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+
+        public void AddSales(SalesRecord salesRecord)
+        {
+            SalesRecords.Add(salesRecord);
+        }
+
+        public void RemoveSales(SalesRecord salesRecord)
+        {
+            SalesRecords.Remove(salesRecord);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return SalesRecords.Where(x => x.Date >= initial && x.Date <= final).Sum(x => x.Amount);
+        }
+    }
+}
